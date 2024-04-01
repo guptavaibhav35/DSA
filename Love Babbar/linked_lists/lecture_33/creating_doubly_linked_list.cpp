91bb733fb1d23eaff82a741428301e44480bc9df
Vaibhav Gupa <vaibhav.gupta@vanderbilt.edu>
2024-03-31 23:37:40 -0500

    creating the linkedlist folder and adding files to it

---
+++ Love Babbar/linked_lists/lecture_33/creating_doubly_linked_list.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int creating_doubly_linked_list()
+{
+    cout << "Helloooooo4545456" << endl;
+    return 0;
+}
---
+++ Love Babbar/linked_lists/lecture_33/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "creating_doubly_linked_list.cpp"
+using namespace std;
+
+int main()
+{
+    creating_doubly_linked_list();
+    return 0;
+}
